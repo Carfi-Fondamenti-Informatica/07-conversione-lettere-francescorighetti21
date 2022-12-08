eaa31fa6e904a8054c43f4295a08162f9fab018f
francescorighetti21 <117826738+francescorighetti21@users.noreply.github.com>
2022-12-08 23:03:50 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 int main(){
  char a=0;
-  cin >>a,
+  cin >>a;
   if(convert(a)){
     cout<< a << endl;
    }else{
   
-  cout << errore << endl;
+  cout << "errore" << endl;
   }
    return 0;
 }
